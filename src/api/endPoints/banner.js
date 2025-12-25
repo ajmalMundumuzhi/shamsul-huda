a23ece7700347aa673f43811ba61650556dd1868
ajmalMundumuzhi <muhajmal73@gmail.com>
2025-12-25 07:57:01 +0530

    feat: fixing production issue

--- src/api/endPoints/banner.js
+++ src/api/endPoints/banner.js
@@ -1,6 +1,6 @@
 import apiClient from "../axiosInstance";
 
 export const fetchBanner = async () => {
-    const response = await apiClient.get('banner')
+    const response = await apiClient.get('/banner')
     return response.data.banners;
 }
--- src/api/endPoints/notification.js
+++ src/api/endPoints/notification.js
@@ -1,6 +1,6 @@
 import apiClient from "../axiosInstance";
 
 export const fetchNotification = async () => {
-    const response = await apiClient.get('notifications')
+    const response = await apiClient.get('/notifications')
     return response.data.notifications;
 }
